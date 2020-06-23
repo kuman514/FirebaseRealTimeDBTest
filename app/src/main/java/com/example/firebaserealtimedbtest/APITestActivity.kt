89a96186c0e415b4bdc02710d30707024892c26c
kumankoishi.py <7iggif@naver.com>
2020-06-23 22:39:42 +0900

    Setting URL (except api key)

--- app/src/main/java/com/example/firebaserealtimedbtest/APITestActivity.kt
+++ app/src/main/java/com/example/firebaserealtimedbtest/APITestActivity.kt
@@ -1,12 +1,23 @@
 package com.example.firebaserealtimedbtest
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_api_test.*
+import java.net.URL
 
 class APITestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_api_test)
+
+        val result = findViewById<TextView>(R.id.testResult)
+
+        val Urlstr = "http://api.visitkorea.or.kr/openapi/service/rest/EngService/searchStay?serviceKey="
+
+        val parm = "&numOfRows=10&pageSize=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&listYN=Y&arrange=A&hanOk=1&_type=json"
+
+        val url = URL(Urlstr + key + parm)
     }
 }
